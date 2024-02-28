abacbd6deb496b8099fe0e549780b189dcc62bdb
vikkuccine <kamenkova09@gmail.com>
2024-02-28 23:20:34 +0200

    finish sendmail

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vikkuccine.saenq.space/portfolio',
+  base: '/portfolio',
   integrations: [tailwind(), sitemap(), astroI18next(), react()]
 });
